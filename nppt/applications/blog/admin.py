118fae124d1bc2ac73aa2671c955f323e6a23806
danieldpg <dangel_13@hotmail.com>
2018-01-08 14:53:02 -0500

    admin y arreglos base de datos

--- nppt/applications/blog/admin.py
+++ nppt/applications/blog/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 from .models import Category, Tag, Blog
 # Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+     'category',
+     'title_seo',
+     'description_seo',
+     'title',
+     'sub_title',
+     'description',
+     'content',
+     'author',
+     'tags',
+    )
+
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Blog)
