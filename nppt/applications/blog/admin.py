0ddeb52de08f7543b8beee96e640da7cc96d7cce
danieldpg <dangel_13@hotmail.com>
2018-01-09 23:38:18 -0500

    admin y nueva tabla social project

--- nppt/applications/blog/admin.py
+++ nppt/applications/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Tag, Blog
+from .models import Category, Tag, Blog, Social_Project
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
@@ -13,12 +13,49 @@ class BlogAdmin(admin.ModelAdmin):
      'description',
      'content',
      'author',
-     'tags',
     )
     list_filter = ('category',)
+    # campos para agregar
+    """fields = (
+        'title',
+        'description',
+        'content',
+        'category',
+        'image1',
+        'tags',
+        'author',
+    )"""
+    filter_horizontal = ('tags',)
+
+
+class SocialProjectAdmin(admin.ModelAdmin):
+    list_display = (
+     'category',
+     'title_seo',
+     'description_seo',
+     'title',
+     'sub_title',
+     'description',
+     'content',
+     'author',
+    )
+    list_filter = ('category',)
+    # campos para agregar
+    """fields = (
+        'title',
+        'description',
+        'content',
+        'category',
+        'image1',
+        'tags',
+        'author',
+    )"""
+    filter_horizontal = ('tags',)
+
 
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Social_Project, SocialProjectAdmin)
 
 
--- nppt/applications/blog/models.py
+++ nppt/applications/blog/models.py
@@ -65,7 +65,7 @@ class Blog(TimeStampedModel):
         blank=True,
         null=True
     )
-    tags = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
     class Meta:
         verbose_name = 'blog'
@@ -74,3 +74,56 @@ class Blog(TimeStampedModel):
 
     def __str__(self):
         return self.title_seo
+
+@python_2_unicode_compatible
+class Social_Project(TimeStampedModel):
+   MUY_ALTO= '3'
+   ALTO = '2'
+   MEDIO = '1'
+   BAJO= '0'
+   MODE_CHOICES = (
+       (MUY_ALTO, 'Muy alto'),
+       (ALTO, 'Alto'),
+       (MEDIO, 'Medio'),
+       (BAJO, 'Bajo'),
+   )
+   # se cambia por choises
+   importance = models.CharField(
+        'importancia',
+        max_length=2,
+        blank=True,
+        choices=MODE_CHOICES
+   )
+   video = models.URLField('movie')
+   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+   title_seo = models.CharField('titulo seo', max_length=100)
+   description_seo = models.CharField('descripcion seo', max_length=200)
+   title = models.CharField('titulo', max_length=100)
+   sub_title = models.CharField('subtitulo', max_length=200)
+   description = models.CharField('descripcion', max_length=200)
+   content = FroalaField()
+   author = models.CharField('autor', max_length=200)
+   image1 = models.ImageField('imagen uno', upload_to="blog")
+   image2 = models.ImageField(
+       'imagen dos',
+       upload_to="blog",
+       blank=True,
+       null=True
+   )
+
+   image3 = models.ImageField(
+       'imagen tres',
+       upload_to="blog",
+       blank=True,
+       null=True
+   )
+   tags = models.ManyToManyField(Tag)
+
+   class Meta:
+       verbose_name = 'Proyeccion social'
+       verbose_name_plural = 'Proyecciones sociales'
+       ordering = ['-created']
+
+   def __str__(self):
+       return self.title_seo
+
